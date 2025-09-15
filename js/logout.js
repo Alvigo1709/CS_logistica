c487c9024ec9e7c6a971173b775a76c6e6dfd0ca
Alvigo1709 <127364982+Alvigo1709@users.noreply.github.com>
2025-09-15 15:03:15 -0500

    Update logout.js

--- js/logout.js
+++ js/logout.js
@@ -1,21 +1,18 @@
 // js/logout.js
 document.addEventListener('DOMContentLoaded', () => {
-  if (document.getElementById('logout-btn')) return;
+    if (document.getElementById('logout-btn')) return;
 
-  const logoutBtn = document.createElement('button');
-  logoutBtn.id = 'logout-btn';
-  logoutBtn.textContent = 'Cerrar Sesión';
-  logoutBtn.classList.add('logout-button');
-  document.body.appendChild(logoutBtn);
+    const logoutBtn = document.createElement('button');
+    logoutBtn.id = 'logout-btn';
+    logoutBtn.textContent = 'Cerrar Sesión';
+    logoutBtn.classList.add('logout-button');
+    document.body.appendChild(logoutBtn);
 
     logoutBtn.addEventListener('click', () => {
-    localStorage.clear();
-    window.location.href = '../login.html'; // cambia a './login.html' si estás en raíz
-     // Evitar volver con el botón atrás
-    window.history.pushState(null, "", window.location.href);
-    window.onpopstate = function () {
-    window.history.go(1);};
-  });
+        // Borrar sesión
+        localStorage.clear();
+
+        // Redirigir reemplazando historial
+        window.location.replace('../login.html');
+    });
 });
-
-
